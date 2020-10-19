007e7502fd84acc3249fec485820031a761e8102
HMMK99 <hatemkamal1999@gmail.com>
2020-10-19 09:08:23 +0200

    Sharing posts forms and views

--- site1/blog/views.py
+++ site1/blog/views.py
@@ -1,23 +1,26 @@
 from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+# from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from .models import Post
+from .forms import EmailPostForm
 
+# Sharing a post with other email
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status='published')
+    # 2- performing the sending
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.clean_data
+            # send the email
+    else:
+        # 1- getting the input from user
+        form = EmailPostForm()
+    return render(request,
+        'blog/post/share.html',
+        {'post': post,
+        'form': form})
 
-# def post_list(request):
-#     object_list = Post.published.all()
-#     paginator = Paginator(object_list, 4) # 4 post/page
-#     page = request.GET.get('page')
-#     try:
-#         posts = paginator.page(page)
-#     except PageNotAnInteger:
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         posts = paginator.page(paginator.num_pages)
-#     return render(request,
-#                 'blog/post/list.html',
-#                 {'page': page,
-#                 'posts': posts})
 
 class PostListView(ListView):
     queryset = Post.published.all()
